9a41b828e8609eb4c70328f66f4ed5121dc6fede
Virtuoso279 <89971473+Virtuoso279@users.noreply.github.com>
2022-04-28 17:42:53 +0300

    Add files via upload

---
+++ OOP/Lab4/Lab4Project3(Task9)/Enterprise.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+
+namespace Lab4interf
+{
+    class Enterprise : IComparable, IComparer, IEnumerable
+    {
+        protected string name;        
+        protected string sphere;
+        protected int numberEmployes;
+        protected int income;
+        protected int rating;        
+
+        //конструктор з параметрами
+        public Enterprise(string name, string sphere, int numberEmployes, int income, int rating)
+        {
+            this.name = name;            
+            this.sphere = sphere;
+            this.numberEmployes = numberEmployes;
+            this.income = income;
+            this.rating = rating;            
+        }
+        public Enterprise() { }
+
+        //властивості з аксесорами
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Rating
+        {
+            get { return rating; }
+            set { rating = value; }
+        }
+        public int NumberEmployes
+        {
+            get { return numberEmployes; }
+            set { numberEmployes = value; }
+        }
+        public int Income
+        {
+            get { return income; }
+            set { income = value; }
+        }
+
+        //метод виводу значень на консоль
+        public void printValuesE()
+        {
+            Console.WriteLine("=== Enterprise ===");
+            Console.WriteLine($"Name: {name}\nSphere: {sphere}\nNumberEmployes: {numberEmployes}\nIncome: {income}\nRating: {rating}");
+        }       
+
+        //методи, які реалізують інтерфейси
+        public int CompareTo(int number)
+        {
+            return number;
+        }
+
+        public int Compare(int value1, int value2)
+        {
+            return value1.CompareTo(value2);
+        }
+
+        public void GetEnumerator(Enterprise[] elem, int numb)
+        {
+            Enterprise temp = new Enterprise();
+            for (int i = 0; i < numb-1; i++)
+            {
+                for (int k = i+1; k < numb; k++)
+                {
+                    if (elem[i].Rating > elem[k].Rating)
+                    {
+                        temp = elem[i];
+                        elem[i] = elem[k];
+                        elem[k] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ OOP/Lab4/Lab4Project3(Task9)/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Lab4interf
+{
+    interface IComparable
+    {
+        int CompareTo(int number);
+    }
+
+    interface IComparer
+    {
+        int Compare(int value1, int value2);
+    }
+
+    interface IEnumerable
+    {
+        void GetEnumerator(Enterprise[] elem, int numb);
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int number = 1;
+            Random element = new Random();
+            Console.WriteLine("Lab 4 (Variant 5) Maiboroda Igor IPZ-11/2");
+            Console.WriteLine(new string('=', 60));
+            Console.WriteLine("Task 9");            
+            Console.Write("Enter number of instances of class: ");
+            while (!int.TryParse(Console.ReadLine(), out number) || number <= 0)
+            {
+                Console.Write("Enter other value: ");
+            }
+            Enterprise[] company = new Enterprise[number];
+            for (int i = 0; i < number; i++)
+            {
+                company[i] = new Enterprise(("company" + i),  ("city" + i), element.Next(1, 50000), element.Next(10000, 100000), 0);                
+                Console.Write($"Input rating of {i} company: ");
+                company[i].Rating = Convert.ToInt32(Console.ReadLine());
+                company[i].printValuesE();
+            }
+
+            //interface IComparable
+            int index1, index2;
+            Console.WriteLine(new string('=', 60));
+            Console.WriteLine("Using interface IComparable");
+            Console.Write("Input indexes of elements of array to compare them: ");
+            while (!int.TryParse(Console.ReadLine(), out index1) || index1 < 0 || index1 >= number)
+            {
+                Console.Write("Enter other value: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out index2) || index2 < 0 || index2 >= number)
+            {
+                Console.Write("Enter other value: ");
+            }
+            //перевірка значень
+            if (company[index1].NumberEmployes.CompareTo(company[index2].NumberEmployes) > 0)
+            {
+                Console.WriteLine($"{company[index1].Name} has more employes than {company[index2].Name}");
+            }
+            else if (company[index1].NumberEmployes.CompareTo(company[index2].NumberEmployes) < 0)
+            {
+                Console.WriteLine($"{company[index2].Name} has more employes than {company[index1].Name}");
+            }
+            else
+            {
+                Console.WriteLine("The numbers of employes are the same");
+            }
+
+            //interface IComparer
+            Console.WriteLine(new string('=', 60));
+            Console.WriteLine("Using interface IComparer");
+            Console.WriteLine("Let's compare the number of employes and income of enterprise");
+            Console.Write("Input index of element of array: ");
+            while (!int.TryParse(Console.ReadLine(), out index1) || index1 < 0 || index1 >= number)
+            {
+                Console.Write("Enter other value: ");
+            }
+            //перевірка значень
+            if (company[index1].Compare(company[index1].NumberEmployes, company[index1].Income) > 0)
+            {
+                Console.WriteLine($"{company[index1].Name} has more employes than income");
+            }
+            else if (company[index1].Compare(company[index1].NumberEmployes, company[index1].Income) < 0)
+            {
+                Console.WriteLine($"{company[index2].Name} has more income than employes");
+            }
+            else
+            {
+                Console.WriteLine("The numbers of employes and income are the same");
+            }
+
+            //interface IEnumerable
+            Console.WriteLine(new string('=', 60));
+            Console.WriteLine("Using interface IEnumerable: sorting by rating");
+            company[0].GetEnumerator(company, number);
+            for (int i = 0; i < number; i++)
+            {                
+                company[i].printValuesE();
+            }
+        }
+    }
+}
