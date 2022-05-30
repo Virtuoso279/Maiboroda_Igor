385b4c804f2d85cb01f8ff15326d57106649e22c
Virtuoso279 <89971473+Virtuoso279@users.noreply.github.com>
2022-05-30 17:44:18 +0300

    Add files via upload

---
+++ OOP/Project/Code/Version5/Group.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project
+{
+    interface IGroup
+    {
+        void printGroupInform();
+        void printGroupPoints(int numberLabWorks, int numberHomeWorks);
+        void printGroupAttending(int semestr, int numberLessons, Subject OOP);
+        void changeAttendance(int numberLessons);
+        void changePoints(int numberLabWorks, int numberHomeWorks);
+        void generatingGroup(Random element);
+    }
+
+    class Group : IGroup
+    {
+        string faculty;
+        string specialty;
+        int courseNumber;
+        string nameGroup;
+        int numberStudents;
+
+        Student[] group;
+
+        //конструктори
+        public Group(string faculty, string specialty, int courseNumber, string nameGroup, int numberStudents)
+        {
+            this.faculty = faculty;
+            this.specialty = specialty;
+            this.courseNumber = courseNumber;
+            this.nameGroup = nameGroup;
+            this.numberStudents = numberStudents;
+            group = new Student[numberStudents];
+            Console.WriteLine($"Група {nameGroup} була створена");
+        }
+        public Student this[int index]
+        {
+            get { return group[index]; }
+            set { group[index] = value; }
+        }
+        public Group()
+        {
+            Console.WriteLine("Пуста група була створена");
+        }
+        public Group(Group previous)
+        {
+            this.faculty = previous.faculty;
+            this.specialty = previous.specialty;
+            this.courseNumber = previous.courseNumber;
+            this.nameGroup = previous.nameGroup;
+            this.numberStudents = previous.numberStudents;
+            group = previous.group;
+            Console.WriteLine("Копія групи була створена");
+        }
+
+        //властивості
+        public string Faculty
+        {
+            get { return faculty; }
+            set { faculty = value; }
+        }
+        public string Specialty
+        {
+            get { return specialty; }
+            set { specialty = value; }
+        }
+        public string NameGroup
+        {
+            get { return nameGroup; }
+            set { nameGroup = value; }
+        }
+        public int CourseNumber
+        {
+            get { return courseNumber; }
+            set
+            {
+                if (value == 1 || value == 2 || value == 3 || value == 4)
+                    courseNumber = value;
+                else
+                    Console.WriteLine("Число має бути від 1 до 4!");
+            }
+        }
+        public int NumberStudents
+        {
+            get { return numberStudents; }
+            set
+            {
+                if (value > 0)
+                    numberStudents = value;
+                else
+                    Console.WriteLine("Число має бути більше 0!");
+            }
+        }
+
+        public void printGroupInform()
+        {
+            Console.WriteLine(new string('-', 70));
+            Console.WriteLine("Інформація про групу");
+            Console.WriteLine("Факультет: " + faculty);
+            Console.WriteLine("Спеціальність: " + specialty);
+            Console.WriteLine("Курс: " + courseNumber);
+            Console.WriteLine("Назва групи: " + nameGroup);
+            Console.WriteLine("Кількість студентів групи: " + numberStudents);
+            Console.WriteLine();
+        }
+
+        public void printGroupPoints(int numberLabWorks, int numberHomeWorks)
+        {
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 70));
+            Console.WriteLine("Журнал оцінок");
+            Console.Write("\t\t");
+            for (int i = 0; i < numberLabWorks; i++)
+            {
+                Console.Write("ЛР" + (i + 1) + "\t");
+            }
+            for (int i = 0; i < numberHomeWorks; i++)
+            {
+                Console.Write("ДЗ" + (i + 1) + "\t");
+            }
+            Console.WriteLine("Ак.\tПр.\tДБ\tСум");
+            for (int i = 0; i < numberStudents; i++)
+            {
+                Console.Write($"{(i + 1)}. " + group[i].fullName + "\t");
+                group[i].allpoints.printMarksStudent();
+            }
+        }
+
+        public void printGroupAttending(int semestr, int numberLessons, Subject OOP)
+        {
+            Console.WriteLine(new string('-', 70));
+            Console.WriteLine("Облік присутності студентів");
+            if (semestr == 1)
+            {
+                OOP.oneSemester.printAllDates(numberLessons);
+                for (int i = 0; i < numberStudents; i++)
+                {
+                    Console.Write($"{(i + 1)}. " + group[i].fullName + "\t");
+                    group[i].allAttendance.printAttendingStudent(semestr);
+                }
+            }
+            else
+            {
+                OOP.otherSemester.printAllDates(numberLessons);
+                for (int i = 0; i < numberStudents; i++)
+                {
+                    Console.Write($"{(i + 1)}. " + group[i].fullName + "\t");
+                    group[i].allAttendance.printAttendingStudent(semestr);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void changeAttendance(int numberLessons)
+        {
+            int studentNumber, lessonNumber, value;
+            Console.WriteLine(new string('-', 70));
+            Console.WriteLine("Заміна відвідувань");
+            Console.Write("Оберіть студента за номером: ");
+            while (!int.TryParse(Console.ReadLine(), out studentNumber) || studentNumber < 1 || studentNumber > 25)
+            {
+                Console.Write("Ведіть число від 1 до 25: ");
+            }
+            Console.Write("Номер заняття: ");
+            while (!int.TryParse(Console.ReadLine(), out lessonNumber) || lessonNumber < 1 || lessonNumber > numberLessons)
+            {
+                Console.Write($"Ведіть число від 1 до {numberLessons}: ");
+            }
+            Console.Write("Присутність (0 або 1): ");
+            while (!int.TryParse(Console.ReadLine(), out value) || (value != 1 && value != 0))
+            {
+                Console.Write("Ведіть 0 або 1: ");
+            }
+            group[studentNumber - 1].allAttendance.AttendingLessons[lessonNumber - 1] = value;
+            group[studentNumber - 1].allAttendance.calculateAttending();
+        }
+
+        public void changePoints(int numberLabWorks, int numberHomeWorks)
+        {
+            int studentNumber, value, workNumber, workArrayNumber;
+            Console.WriteLine(new string('-', 70));
+            Console.WriteLine("Заміна оцінок");
+            Console.WriteLine("1 - лабораторна робота, 2 - домашнє завдання, 3 - активність, 4 - проект, 5 - додаткові бали");
+            Console.Write("Оберіть студента за номером: ");
+            while (!int.TryParse(Console.ReadLine(), out studentNumber) || studentNumber < 1 || studentNumber > 25)
+            {
+                Console.Write("Ведіть число від 1 до 25: ");
+            }
+            Console.Write("Оберіть вид робіт: ");
+            while (!int.TryParse(Console.ReadLine(), out workNumber) || workNumber < 1 || workNumber > 5)
+            {
+                Console.Write("Ведіть число від 1 до 5: ");
+            }
+            Console.Write("Внесість номер ЛР або ДЗ (за необхідності): ");
+            while (!int.TryParse(Console.ReadLine(), out workArrayNumber) || workArrayNumber < 1 || (workArrayNumber > numberLabWorks && workArrayNumber > numberHomeWorks))
+            {
+                Console.Write("Ведіть інше число: ");
+            }
+            Console.Write("Внесіть оцінку: ");
+            while (!int.TryParse(Console.ReadLine(), out value) || value < 1)
+            {
+                Console.Write("Ведіть число від 1 до 5: ");
+            }
+
+            switch (workNumber)
+            {
+                case 1: group[studentNumber - 1].allpoints.LabWorks[workArrayNumber - 1] = value; break;
+                case 2: group[studentNumber - 1].allpoints.HomeWorks[workArrayNumber - 1] = value; break;
+                case 3: group[studentNumber - 1].allpoints.Activity = value; break;
+                case 4: group[studentNumber - 1].allpoints.Project = value; break;
+                case 5: group[studentNumber - 1].allpoints.ExtraPoints = value; break;
+            }
+            group[studentNumber - 1].allpoints.calculateSumPoints();
+        }
+
+        public void generatingGroup(Random element)
+        {
+            int maxLabMark, maxHomeWorkMark, maxProjectMark, maxActivity, maxExtraPoints;
+            Console.Write("Введіть максимальний бал за лабораторну роботу: ");
+            while (!int.TryParse(Console.ReadLine(), out maxLabMark) || maxLabMark < 0)
+            {
+                Console.Write("Ведіть число більше 0: ");
+            }
+            Console.Write("Введіть максимальний бал за домашню роботу: ");
+            while (!int.TryParse(Console.ReadLine(), out maxHomeWorkMark) || maxHomeWorkMark < 0)
+            {
+                Console.Write("Ведіть число більше 0: ");
+            }
+            Console.Write("Введіть максимальний бал за проект: ");
+            while (!int.TryParse(Console.ReadLine(), out maxProjectMark) || maxProjectMark < 0)
+            {
+                Console.Write("Ведіть число більше 0: ");
+            }
+            Console.Write("Введіть максимальний бал за активність на лекціях: ");
+            while (!int.TryParse(Console.ReadLine(), out maxActivity) || maxActivity < 0)
+            {
+                Console.Write("Ведіть число більше 0: ");
+            }
+            Console.Write("Введіть максимальний бал за додаткову роботу: ");
+            while (!int.TryParse(Console.ReadLine(), out maxExtraPoints) || maxExtraPoints < 0)
+            {
+                Console.Write("Ведіть число більше 0: ");
+            }
+
+            for (int i = 0; i < 25; i++)
+            {
+                group[i].allAttendance.randomAttending(element);
+                group[i].allpoints.randomMarks(element, maxLabMark, maxHomeWorkMark, maxProjectMark, maxActivity, maxExtraPoints);
+            }
+        }
+    }
+}
---
+++ OOP/Project/Code/Version5/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Майборода Ігор Сергійович ІПЗ-11");
+            Console.WriteLine("Проект №46. Версія 5");
+            Console.WriteLine("Старт");
+            Console.WriteLine(new string('=', 100));
+            menu();
+            Console.WriteLine();
+            Console.WriteLine(new string('=', 100));
+            Console.WriteLine("Фініш");
+        }
+
+        static void menu()
+        {
+            //===============================початок================================================
+            int numberLessons, semester, choosing;
+            Random element = new Random();
+            Console.WriteLine(new string(' ', 30) + " Генерація даних");
+            Console.Write("Оберіть семестр: ");
+            while (!int.TryParse(Console.ReadLine(), out semester) || (semester != 1 && semester != 2))
+            {
+                Console.Write("Ведіть 1 або 2: ");
+            }
+            Console.Write("Введіть загальну кількість занять: ");
+            while (!int.TryParse(Console.ReadLine(), out numberLessons) || numberLessons <= 0)
+            {
+                Console.Write("Ведіть число більше 0: ");
+            }
+
+            //=============================предмет та дати занять======================================            
+            Subject OOP = new Subject("Вступ до ООП", "Бичков О. С.", numberLessons);
+            OOP.printSubject();
+            if (semester == 1)
+            {
+                OOP.oneSemester.generateRandomDates(numberLessons, element);
+                OOP.oneSemester.printDates(numberLessons);
+                Console.Write("\nВас влаштовують створені дати? (Так = 1, Ні = 0): ");
+                while (!int.TryParse(Console.ReadLine(), out choosing) || (choosing != 1 && choosing != 0))
+                {
+                    Console.Write("Ведіть 1 або 0: ");
+                }
+                if (choosing == 0)
+                {
+                    OOP.oneSemester.changeDates();
+                    OOP.oneSemester.printDates(numberLessons);
+                }
+            }
+            else
+            {
+                OOP.otherSemester.generateRandomDates(numberLessons, element);
+                OOP.otherSemester.printDates(numberLessons);
+                Console.Write("\nВас влаштовують створені дати? (Так = 1, Ні = 0): ");
+                while (!int.TryParse(Console.ReadLine(), out choosing) || (choosing != 1 && choosing != 0))
+                {
+                    Console.Write("Ведіть 1 або 0: ");
+                }
+                if (choosing == 0)
+                {
+                    OOP.otherSemester.changeDates();
+                    OOP.otherSemester.printDates(numberLessons);
+                }
+            }
+            Console.ReadKey();
+
+            //=============================група студентів, оцінки, присутність======================================
+            int numberLabWorks, numberHomeWorks;
+            string[] fullnames = { "Акуліч Д.", "Боєчко В.", "Булава Г.", "Гавриленко", "Дармороз", "Діжурко І.", "Зганяйко", "Ковальчук", "Корсун М.", "Кузнецова", "Литвинчук", "Майборода", "Манойлова", "Мартинкова", "Павлюченко", "Радін Н.", "Романова", "Рябиця М.", "Самойленко", "Саніна В.", "Сирота А.", "Травіна А.", "Тухар Р.", "Шкітак Н.", "Шимон Ю." };
+            Console.WriteLine();
+            Group IPZ11 = new Group("ФІТ", "ІПЗ", 1, "ІПЗ-11", 25);
+            IPZ11.printGroupInform();
+            Console.Write("Введіть кількість лабораторних: ");
+            while (!int.TryParse(Console.ReadLine(), out numberLabWorks) || numberLabWorks < 0)
+            {
+                Console.Write("Ведіть число більше 0: ");
+            }
+            Console.Write("Введіть кількість домашніх завдань: ");
+            while (!int.TryParse(Console.ReadLine(), out numberHomeWorks) || numberHomeWorks < 0)
+            {
+                Console.Write("Ведіть число більше 0: ");
+            }
+
+            Points[] marks = new Points[25];
+            Attendance[] attending = new Attendance[25];
+            for (int i = 0; i < 25; i++)
+            {
+                marks[i] = new Points(numberLabWorks, numberHomeWorks);
+                attending[i] = new Attendance(numberLessons);
+                IPZ11[i] = new Student(marks[i], attending[i], fullnames[i]);
+            }
+
+            IPZ11.generatingGroup(element);
+            Console.ReadKey();
+            IPZ11.printGroupAttending(semester, numberLessons, OOP);
+            Console.ReadKey();
+            IPZ11.printGroupPoints(numberLabWorks, numberHomeWorks);
+
+            //=============================заміна інформації==========================================
+            int studentNumb, studentNumb2; choosing = 1;
+            Console.WriteLine();
+            Console.WriteLine(new string('=', 100));
+            Console.WriteLine(new string(' ', 30) + " Заміна даних");
+            while (choosing != 0)
+            {
+                Console.WriteLine("Меню");
+                Console.WriteLine("1. Робота з обліком присутності.");
+                Console.WriteLine("2. Робота з журналом оцінок.");
+                Console.WriteLine("3. Отримати відомості про студента до іспиту.");
+                Console.WriteLine("4. Отримати відомості про студента після іспиту.");
+                Console.WriteLine("5. Робота з операторами.");
+                Console.WriteLine("6. Перегляд обліку присутності та журналу оцінок.");
+                Console.Write("Введіть номер завдання (0 щоб зупинити): ");
+                while (!int.TryParse(Console.ReadLine(), out choosing) || (choosing < 0 && choosing > 6))
+                {
+                    Console.Write("Ведіть від 1 до 6 (0 щоб зупинити): ");
+                }
+                switch (choosing)
+                {
+                    case 1: IPZ11.changeAttendance(numberLessons); break;
+                    case 2: IPZ11.changePoints(numberLabWorks, numberHomeWorks); break;
+                    case 3:
+                        {
+                            Console.Write("Оберіть номер студента: ");
+                            while (!int.TryParse(Console.ReadLine(), out studentNumb) || studentNumb < 1 || studentNumb > 25)
+                            {
+                                Console.Write("Ведіть число від 1 до 25: ");
+                            }
+                            IPZ11[studentNumb - 1].informBeforeExam(IPZ11); break;
+                        }
+                    case 4:
+                        {
+                            Console.Write("Оберіть номер студента: ");
+                            while (!int.TryParse(Console.ReadLine(), out studentNumb) || studentNumb < 1 || studentNumb > 25)
+                            {
+                                Console.Write("Ведіть число від 1 до 25: ");
+                            }
+                            IPZ11[studentNumb - 1].ExamPoints = element.Next(0, 41);
+                            IPZ11[studentNumb - 1].informAfterExam(IPZ11); break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Порівняння двох студентів за успішністю");
+                            Console.Write("Оберіть 1-ого студента: ");
+                            while (!int.TryParse(Console.ReadLine(), out studentNumb) || studentNumb < 1 || studentNumb > 25)
+                            {
+                                Console.Write("Ведіть число від 1 до 25: ");
+                            }
+                            Console.Write("Оберіть 2-ого студента: ");
+                            while (!int.TryParse(Console.ReadLine(), out studentNumb2) || studentNumb2 < 1 || studentNumb2 > 25)
+                            {
+                                Console.Write("Ведіть число від 1 до 25: ");
+                            }
+                            bool best = IPZ11[studentNumb - 1] > IPZ11[studentNumb2 - 1];
+                            if (best)
+                                Console.WriteLine($"{IPZ11[studentNumb - 1].fullName} більш успішний студент, ніж {IPZ11[studentNumb2 - 1].fullName}");
+                            else
+                                Console.WriteLine($"{IPZ11[studentNumb2 - 1].fullName} більш успішний студент, ніж {IPZ11[studentNumb - 1].fullName}");
+                            Console.WriteLine("Збільшуємо поточний бал у першого студента на 5 і зменшуємо у другого на 5");
+                            ++IPZ11[studentNumb - 1]; --IPZ11[studentNumb2 - 1];
+                            IPZ11[studentNumb - 1].informBeforeExam(IPZ11);
+                            IPZ11[studentNumb2 - 1].informBeforeExam(IPZ11);
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.ReadKey();
+                            IPZ11.printGroupAttending(semester, numberLessons, OOP);
+                            Console.ReadKey();
+                            IPZ11.printGroupPoints(numberLabWorks, numberHomeWorks); break;
+                        }
+                }
+            }
+        }
+    }
+}
